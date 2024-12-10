e4846c0cce2e41f6b1424b6785c269193bad9255
backpackerdeveloper <contactshubhamtripz@gmail.com>
2024-12-10 13:15:10 +0530

    collected viewmodel data in MainActivity

--- app/src/main/java/com/myjar/jarassignment/MainActivity.kt
+++ app/src/main/java/com/myjar/jarassignment/MainActivity.kt
@@ -4,20 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.myjar.jarassignment.databinding.ActivityMainBinding
+import com.myjar.jarassignment.ui.adapter.RecyclerAdapter
 import com.myjar.jarassignment.ui.vm.JarViewModel
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
     private val viewModel by viewModels<JarViewModel>()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
+        lifecycleScope.launch {
+            viewModel.listStringData.collect{ items ->
+                binding.recyclerView.adapter = RecyclerAdapter(items)
+            }
+        }
+
+
     }
 
-    override fun onResume() {
-        super.onResume()
-        viewModel.fetchData()
-    }
 }
